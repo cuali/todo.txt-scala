3e1b356a0b7f5b1d8eb2497013aa2839fdad2b33
Jan Ouwens <jan.ouwens@gmail.com>
2015-10-30 14:43:56 +0100

    Parse several tasks at once

--- README.md
+++ README.md
@@ -3,6 +3,3 @@
 This basic parser for [todo.txt](https://github.com/ginatrapani/todo.txt-cli/wiki/The-Todo.txt-Format) format 
 is written with [parboiled2](https://github.com/sirthias/parboiled2) in Scala.
 
-## Limitation
-
-It currently accepts only one single line at a time.
--- src/main/scala/cua/li/ti/todo/TodoParser.scala
+++ src/main/scala/cua/li/ti/todo/TodoParser.scala
@@ -7,6 +7,9 @@ import org.parboiled2.{ CharPredicate, Parser, ParserInput }
  * https://github.com/ginatrapani/todo.txt-cli/wiki/The-Todo.txt-Format
  */
 case class TodoParser(val input: ParserInput) extends Parser {
+  def tasks = rule { push(Seq.empty[Task]) ~ zeroOrMore(task ~> {
+    (s: Seq[Task], t: Task) => s :+ t
+  }) ~ EOI }
   def task = rule {
     push(Task()) ~ zeroOrMore(!(" @" | " +") ~ " ") ~
     optional(completed) ~> {
@@ -18,22 +21,23 @@ case class TodoParser(val input: ParserInput) extends Parser {
     } ~ optional(capture(date)) ~> {
       (t: Task, c) =>
         t.copy(created = c)
-    } ~ body ~ EOI }
+    } ~ body ~ optional(EOL) }
   private def priority = rule { "(" ~ capture("A" - "Z") ~ ") " } // Rule 1
   private def completed = rule { "x " ~ capture(date) } // Completed task
   private def date = rule { digits(4) ~ "-" ~ digits(2) ~ "-" ~ digits(2) ~ " " } // Rule 2
   private def digits(n: Int) = rule { n times CharPredicate.Digit }
   private def body = rule { oneOrMore(context | project | freedoc) } // Rule 3
-  private def context = rule { " @" ~ capture(oneOrMore(noneOf(" "))) ~> {
+  private def context = rule { " @" ~ capture(oneOrMore(noneOf(" \r\n"))) ~> {
     (t: Task, c) =>
       t.copy(contexts = t.contexts + Context(c))
   }}
-  private def project = rule { " +" ~ capture(oneOrMore(noneOf(" "))) ~> {
+  private def project = rule { " +" ~ capture(oneOrMore(noneOf(" \r\n"))) ~> {
     (t: Task, p) =>
       t.copy(projects = t.projects + Project(p))
   }}
-  private def freedoc = rule { capture(oneOrMore(!(" @" | " +") ~ ANY)) ~> {
+  private def freedoc = rule { capture(oneOrMore(!(" @" | " +") ~ noneOf("\r\n"))) ~> {
     (t: Task, d) =>
       t.copy(description = Some(t.description.getOrElse("") + d))
   }}
+  private def EOL = rule { "\r\n" | "\n" }
 }
